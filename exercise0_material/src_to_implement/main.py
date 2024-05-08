3c1d221c33c72636e9419aba8791de56e5930138
crazz-zaac <r.alpha919@gmail.com>
2024-05-08 07:28:49 +0200

    code refactoring

---
+++ exercise0_material/src_to_implement/main.py
@@ -0,0 +1,36 @@
+from pattern import Checker, Circle, Spectrum
+
+if __name__ == "__main__":
+
+    checker_board = Checker(6250, 250)
+    checker_board.draw()
+    checker_board.show()
+
+    # circle = Circle(6250, 250, (3125, 3125))
+    # circle.draw()
+    # circle.show()
+
+    # spectrum = Spectrum(5)
+    # spectrum.draw()
+    # spectrum.show()
+
+    # #test checkers
+    # test_checkers = TestCheckers()
+    # test_checkers.setUp()
+    # test_checkers.testPattern()
+    # test_checkers.testPatternDifferentSize()
+    # test_checkers.testReturnCopy
+
+    # #test circle
+    # test_circle = TestCircle()
+    # test_circle.setUp()
+    # test_circle.testPattern()
+    # test_circle.testPatternDifferentSize()
+    # test_circle.testReturnCopy
+
+    # test spectrum
+    # test_spectrum = TestSpectrum()
+    # test_spectrum.setUp()
+    # test_spectrum.testPattern()
+    # test_spectrum.testPatternDifferentSize()
+    # test_spectrum.testReturnCopy
