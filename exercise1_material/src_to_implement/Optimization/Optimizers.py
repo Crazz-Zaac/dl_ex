9e6ac31e88479a7d0d6144bd23157ac10b9cb50d
crazz-zaac <r.alpha919@gmail.com>
2024-05-14 09:20:24 +0200

    class initialization and created calculated_update method

--- exercise1_material/src_to_implement/Optimization/Optimizers.py
+++ exercise1_material/src_to_implement/Optimization/Optimizers.py
@@ -2,12 +2,25 @@ import numpy as np
 from src_to_implement.Optimization.Optimizer import Optimizer
 
 
+
+# Basic optimizer that performs a single step of gradient descent
+# on the given weight tensor.
 class Sgd:
+    '''
+    Performs a single step of gradient descent on the given weight tensor.
+    
+    Args:
+        learning_rate: The learning rate to use.
+    
+    Returns:
+        The updated weight tensor.
+    '''
+    
     def __init__(self, learning_rate: float) -> None:
         self.learning_rate = learning_rate
 
-    def update(self, w: np.ndarray, grad_wrt_w: np.ndarray) -> np.ndarray:
-        return w - self.learning_rate * grad_wrt_w
+    def calculate_update(
+        self, weight_tensor: np.ndarray, gradient_tensor: np.ndarray
+    ) -> np.ndarray:
+        return weight_tensor - self.learning_rate * gradient_tensor
 
-    def get_name(self):
-        return "sgd"
