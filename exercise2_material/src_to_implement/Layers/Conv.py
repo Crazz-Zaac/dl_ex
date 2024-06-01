fe8ea78b4ac5d82ee6d8ee248da9381d7466e945
crazz-zaac <r.alpha919@gmail.com>
2024-06-01 07:56:21 +0200

    initialization

--- exercise2_material/src_to_implement/Layers/Conv.py
+++ exercise2_material/src_to_implement/Layers/Conv.py
@@ -1,14 +1,19 @@
 import numpy as np
 from typing import Union
+from Layers.Base import BaseLayer
 
-
-class Conv:
-
+class Conv(BaseLayer):
+    
     def __init__(
-        self, stride_shape: Union[tuple, int], convolution_shape: tuple, num_kernels: bool
+        self, stride_shape: Union[tuple, int], convolution_shape: tuple, num_kernels: int
     ) -> None:
+        super().__init__()
+        self.trainable = True
         self.stride_shape = stride_shape
         self.convolution_shape = convolution_shape
         self.num_kernels = num_kernels
         self.weights = None
         self.bias = None
+    
+    
+    
