a5e42377c3461747e1ef4b926c133dcf8c3337ff
crazz-zaac <r.alpha919@gmail.com>
2024-06-08 08:37:10 +0200

    building a CNN

--- exercise2_material/src_to_implement/Layers/Conv.py
+++ exercise2_material/src_to_implement/Layers/Conv.py
@@ -2,24 +2,36 @@ import numpy as np
 from typing import Union
 from Layers.Base import BaseLayer
 
+
 class Conv(BaseLayer):
-    
+
     def __init__(
-        self, stride_shape: Union[tuple, int], convolution_shape: tuple, num_kernels: int
+        self,
+        stride_shape: Union[tuple, int],
+        convolution_shape: tuple,
+        num_kernels: int,
     ) -> None:
         super().__init__()
         self.trainable = True
         self.stride_shape = stride_shape
         self.convolution_shape = convolution_shape
-        self.num_kernels = num_kernels
-        self.weights = None
-        self.bias = None
-    
-    
-    def initialize(self, input_shape: tuple) -> None:
-        self.input_shape = input_shape
-        self.weights = np.random.randn(
-            self.num_kernels, self.convolution_shape[0], self.convolution_shape[1]
-        )
-        self.bias = np.random.randn(self.num_kernels)
-        self.output_shape = self.compute_output_shape()
+        self.num_kernels = num_kernels # also called num_filters
+        
+        # Initialize weights for 1D and 2D convolution
+        if len(convolution_shape) == 2:
+            self.weights = np.random.randn(
+                num_kernels, convolution_shape[0], convolution_shape[1])
+        if len(convolution_shape) == 3:
+            self.weights = np.random.randn(
+                num_kernels, convolution_shape[0], convolution_shape[1], convolution_shape[2])
+            
+        self._gradient_weights = None
+        self._bias = None
+
+    @property
+    def gradient_weights(self) -> np.ndarray:
+        return self._gradient_weights
+
+    @property
+    def bias(self) -> np.ndarray:
+        return self._bias
