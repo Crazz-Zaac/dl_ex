2961468e3dedfaa2a298be780493c92085ce6ede
crazz-zaac <r.alpha919@gmail.com>
2024-07-05 12:39:21 +0200

    code refactoring and documentation

--- exercise3_material/src_to_implement/Layers/BatchNormalization.py
+++ exercise3_material/src_to_implement/Layers/BatchNormalization.py
@@ -5,7 +5,49 @@ from Layers.Helpers import compute_bn_gradients
 
 
 class BatchNormalization(BaseLayer):
-    def __init__(self, channels: int):
+    """
+    This class implements the batch normalization layer.
+    It inherits from the BaseLayer class and implements the forward and backward pass.
+
+    Attributes:
+        channels: int
+            number of channels in the input tensor
+        epsilon: float
+            small value to avoid division by zero
+        test_mean: np.ndarray
+            mean of the input tensor during testing phase
+        test_var: np.ndarray
+            variance of the input tensor during testing phase
+        x_tilde: np.ndarray
+            normalized input tensor
+        mean: np.ndarray
+            mean of the input tensor
+        var: np.ndarray
+            variance of the input tensor
+        _gradient_bias: np.ndarray
+            gradient tensor with respect to the bias
+        _gradient_weights: np.ndarray
+            gradient tensor with respect to the weights
+        _optimizer: object
+            optimizer to update the weights
+        _bias_optimizer: object
+            optimizer to update the bias
+
+    Methods:
+        initialize:
+            It initializes the weights and bias of the batch normalization layer.
+
+        forward:
+            Computes the forward pass for the batch normalization layer.
+
+        backward:
+            Computes the backward pass for the batch normalization layer.
+
+        reformat:
+            It reshapes the tensor to the required shape.
+    """
+
+    def __init__(self, channels: int) -> None:
         super().__init__()
         self.channels = channels
         self.trainable = True
@@ -29,43 +71,79 @@ class BatchNormalization(BaseLayer):
         self._optimizer = None
         self._bias_optimizer = None
 
+    # 
     @property
     def bias_optimizer(self):
         return self._bias_optimizer
 
     @bias_optimizer.setter
-    def bias_optimizer(self, opt):
-        self._bias_optimizer = opt
-
+    def bias_optimizer(self, bias_optimizer):
+        self._bias_optimizer = bias_optimizer
+        
+    # property optimizer to get and set the optimizer
     @property
     def optimizer(self):
         return self._optimizer
 
     @optimizer.setter
-    def optimizer(self, opt):
-        self._optimizer = opt
+    def optimizer(self, optimizer):
+        self._optimizer = optimizer
 
+    # property gradient_weights to get and set the gradient_weights during the backward pass
     @property
     def gradient_weights(self):
         return self._gradient_weights
 
     @gradient_weights.setter
-    def gradient_weights(self, x):
-        self._gradient_weights = x
+    def gradient_weights(self, gradient_weights):
+        self._gradient_weights = gradient_weights
 
+    # property gradient_bias to get and set the gradient_bias during the backward pass
     @property
     def gradient_bias(self):
         return self._gradient_bias
 
     @gradient_bias.setter
-    def gradient_bias(self, x):
-        self._gradient_bias = x
-
-    def initialize(self, weights_initializer, bias_initializer):
+    def gradient_bias(self, gradient_bias):
+        self._gradient_bias = gradient_bias
+        
+    # initialize the weights and bias of the batch normalization layer
+    def initialize(self, weights_initializer: object, bias_initializer: object) -> None:
         self.weights = np.ones(self.channels)
         self.bias = np.zeros(self.channels)
 
     def forward(self, input_tensor: np.ndarray) -> np.ndarray:
+        """
+        Computes the forward pass for the batch normalization layer.
+        The formula for batch normalization is:
+        x_tilde = (x - mean) / sqrt(var + epsilon)
+        y = weights * x_tilde + bias
+        if it is not convolution:
+            if testing_phase:
+                x_tilde = (input_tensor - test_mean) / sqrt(test_var + epsilon)
+            else:
+                test_mean = alpha * mean + (1 - alpha) * mean
+                test_var = alpha * var + (1 - alpha) * var
+                x_tilde = (input_tensor - mean) / sqrt(var + epsilon)
+        else:
+            if testing_phase:
+                y = (input_tensor - test_mean) / sqrt(test_var + epsilon)
+            else:
+                new_mean = mean(input_tensor)
+                new_var = var(input_tensor)
+                test_mean = alpha * mean + (1 - alpha) * new_mean
+                test_var = alpha * var + (1 - alpha) * new_var
+                x_tilde = (input_tensor - mean) / sqrt(var + epsilon)
+        
+        Args:
+            input_tensor: np.ndarray
+                The input tensor for the forward pass.
+                
+        Returns:
+            np.ndarray
+                The output tensor after applying the batch normalization layer.
+        """
+        
         self.input_tensor = input_tensor
 
         is_conv = len(input_tensor.shape) == 4
@@ -119,6 +197,56 @@ class BatchNormalization(BaseLayer):
             ) * self.x_tilde + self.bias.reshape((1, channels, 1, 1))
 
     def backward(self, error_tensor: np.ndarray) -> np.ndarray:
+        """
+        Computes the backward pass for the batch normalization layer.
+        If it is not convolution:
+            if testing_phase:
+                dy = error_tensor * weights
+                dx_tilde = dy
+                dvar = np.sum(dy * (input_tensor - test_mean) * (-0.5) * (test_var + epsilon) ** -1.5, axis=axis)
+                dmean = np.sum(dy * (-1) / np.sqrt(test_var + epsilon), axis=axis) + dvar * np.sum(-2 * (input_tensor - test_mean), axis=axis) / batch
+                dx = dx_tilde / np.sqrt(test_var + epsilon) + dvar * 2 * (input_tensor - test_mean) / batch + dmean / batch
+                dgamma = np.sum(error_tensor * x_tilde, axis=axis)
+                dbeta = np.sum(error_tensor, axis=axis)
+            else:
+                dy = error_tensor * weights
+                dx_tilde = dy
+                dvar = np.sum(dy * (input_tensor - mean) * (-0.5) * (var + epsilon) ** -1.5, axis=axis)
+                dmean = np.sum(dy * (-1) / np.sqrt(var + epsilon), axis=axis) + dvar * np.sum(-2 * (input_tensor - mean), axis=axis) / batch
+                dx = dx_tilde / np.sqrt(var + epsilon) + dvar * 2 * (input_tensor - mean) / batch + dmean / batch
+                dgamma = np.sum(error_tensor * x_tilde, axis=axis)
+                dbeta = np.sum(error_tensor, axis=axis)
+        else:
+            if testing_phase:
+                dy = error_tensor
+                dx_tilde = dy
+                dvar = np.sum(dy * (input_tensor - test_mean) * (-0.5) * (test_var + epsilon) ** -1.5, axis=axis)
+                dmean = np.sum(dy * (-1) / np.sqrt(test_var + epsilon), axis=axis) + dvar * np.sum(-2 * (input_tensor - test_mean), axis=axis) / batch
+                dx = dx_tilde / np.sqrt(test_var + epsilon) + dvar * 2 * (input_tensor - test_mean) / batch + dmean / batch
+                dgamma = np.sum(error_tensor * x_tilde, axis=axis)
+                dbeta = np.sum(error_tensor, axis=axis)
+            else:
+                dy = error_tensor
+                dx_tilde = dy
+                dvar = np.sum(dy * (input_tensor - mean) * (-0.5) * (var + epsilon) ** -1.5, axis=axis)
+                dmean = np.sum(dy * (-1) / np.sqrt(var + epsilon), axis=axis) + dvar * np.sum(-2 * (input_tensor - mean), axis=axis) / batch
+                dx = dx_tilde / np.sqrt(var + epsilon) + dvar * 2 * (input_tensor - mean) / batch + dmean / batch
+                dgamma = np.sum(error_tensor * x_tilde, axis=axis)
+                dbeta = np.sum(error_tensor, axis=axis)
+        
+        if optimizer:
+            update weights using gradient with respect to weights
+        if bias_optimizer:
+            update bias using gradient with respect to bias
+        
+        Args:
+            error_tensor: np.ndarray
+                The error tensor for the backward pass.
+                
+        Returns:
+            np.ndarray
+                The error tensor after applying the backward pass.
+        """
 
         axis = 0 if not self.is_conv else (0, 2, 3)
 
@@ -158,12 +286,26 @@ class BatchNormalization(BaseLayer):
         return err_here
 
     def reformat(self, tensor: np.ndarray) -> np.ndarray:
+        """
+        It reshapes the tensor to the required shape. If the input tensor is not convolutional, it reshapes the tensor
+        to the required shape. If the input tensor is convolutional, it reshapes the tensor to the required shape.
         
+        Args:
+            tensor: np.ndarray
+                The input tensor to be reshaped.
+                
+        Returns:
+            np.ndarray
+                The reshaped tensor.
+        """
+
         if len(tensor.shape) == 4:
             batch, channels, height, width = tensor.shape
             reshaped_tensor = tensor.reshape((batch, channels, height * width))
             transposed_tensor = reshaped_tensor.transpose((0, 2, 1))
-            output_tensor = transposed_tensor.reshape((batch * height * width, channels))
+            output_tensor = transposed_tensor.reshape(
+                (batch * height * width, channels)
+            )
         else:
             batch, channels, height, width = self.input_tensor.shape
             reshaped_tensor = tensor.reshape((batch, height * width, channels))
