fabb5bf1d2fd95585a45f46d8a39e49580fc69fe
crazz-zaac <r.alpha919@gmail.com>
2024-07-19 11:25:14 +0200

    data loading and fetching

--- exercise4_material/src_to_implement/data.py
+++ exercise4_material/src_to_implement/data.py
@@ -6,10 +6,36 @@ from skimage.color import gray2rgb
 import numpy as np
 import torchvision as tv
 
+from typing import Tuple
+
 train_mean = [0.59685254, 0.59685254, 0.59685254]
 train_std = [0.16043035, 0.16043035, 0.16043035]
 
 
 class ChallengeDataset(Dataset):
-    # TODO implement the Dataset class according to the description
-    pass
+    
+    def __init__(self, data: pd.DataFrame, mode: str) ->None:
+        self.data = data
+        self.mode = mode
+        self.transform = tv.transforms.Compose([
+            tv.transforms.ToPILImage(),
+            tv.transforms.Resize((128, 128)),
+            tv.transforms.ToTensor(),
+            tv.transforms.Normalize(mean=train_mean, std=train_std)
+        ])
+    
+    def __len__(self) -> int:
+        return len(self.data)
+    
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, int]:
+        img_path = self.data.iloc[idx, 0]
+        img = imread(img_path)
+        if len(img.shape) == 2:
+            img = gray2rgb(img)
+        img = self.transform(img)
+        label = self.data.iloc[idx, 1]
+        return img, label
+
+    
+    
+        
