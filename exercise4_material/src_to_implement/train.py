a0c1dbff225cee40c2aeac77bc8064ecfab654da
crazz-zaac <r.alpha919@gmail.com>
2024-07-21 09:48:45 +0200

    model train

--- exercise4_material/src_to_implement/train.py
+++ exercise4_material/src_to_implement/train.py
@@ -10,21 +10,28 @@ from sklearn.model_selection import train_test_split
 
 # load the data from the csv file and perform a train-test-split
 # this can be accomplished using the already imported pandas and sklearn.model_selection modules
-# TODO
+data = pd.read_csv('data.csv')
+train_data, val_data = train_test_split(data, test_size=0.2, random_state=42)
 
 # set up data loading for the training and validation set each using t.utils.data.DataLoader and ChallengeDataset objects
-# TODO
+train_dataset = ChallengeDataset(train_data, 'train')
+val_dataset = ChallengeDataset(val_data, 'val')
+
+train_loader = t.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = t.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=False)
 
 # create an instance of our ResNet model
-# TODO
+model_instance = model.ResNet()
 
 # set up a suitable loss criterion (you can find a pre-implemented loss functions in t.nn)
 # set up the optimizer (see t.optim)
 # create an object of type Trainer and set its early stopping criterion
-# TODO
+loss_criterion = t.nn.CrossEntropyLoss()
+optimizer = t.optim.Adam(model_instance.parameters(), lr=0.001)
+trainer = Trainer(model=model_instance, crit=loss_criterion, optim=optimizer, train_dl=train_loader, val_test_dl=val_loader, cuda=t.cuda.is_available(), early_stopping_patience=10)
 
 # go, go, go... call fit on trainer
-res = #TODO
+res = trainer.fit(epochs=50)
 
 # plot the results
 plt.plot(np.arange(len(res[0])), res[0], label='train loss')
